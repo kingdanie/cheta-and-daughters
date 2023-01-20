cfb16516e0d05a025885897451db63f1f7594b60
Daniel <52611355+kingdanie@users.noreply.github.com>
2023-01-20 02:21:20 +0000

    add map component

---
+++ components/Maps.vue
@@ -0,0 +1,15 @@
+<template>
+
+    <div class="maps">
+        Maps
+    </div>
+</template>
+
+<style>
+.maps {
+    min-height: 400px;
+    width: 100%;
+    background: red;
+    background-image: url('/img/guyana.png')
+}
+</style>
