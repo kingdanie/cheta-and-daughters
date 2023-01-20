3a2b140047047067f67c238ede5874be289dd7f7
Daniel <52611355+kingdanie@users.noreply.github.com>
2023-01-20 16:32:13 +0000

    cleaning code

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -6,12 +6,6 @@
 </div>
 
 <div class="bg-cyan-600 text-white flex-1 space-y-1 h-full pt-10">
-    <!-- <div v-for="menu in Menu">
-        <div class="p-5 hover:bg-cyan-400">
-            <NuxtLink :to="menu.name"> <span> img </span> {{menu.name}} </NuxtLink>
-        </div>
-        <div>{{ menu.name }}</div>
-    </div> -->
     <NuxtLink v-for="item in Menu" :key="item.name" :href="item.name" :class="[item.current ? 'bg-cyan-500 border-white text-white' : 'border-transparent text-gray-200 hover:bg-gray-50 hover:text-gray-900', 'group capitalize flex items-center px-3 py-2 text-sm font-medium border-l-4']">
           <component :is="item.icon" :class="[item.current ? 'text-gray-50' : 'text-gray-200 group-hover:text-gray-500', 'mr-3 flex-shrink-0 h-6 w-6']" aria-hidden="true" />
           {{ item.name }}
