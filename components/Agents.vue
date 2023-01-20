4502d0a4c281335138eabc2487b2f53d4fc2be32
Daniel <52611355+kingdanie@users.noreply.github.com>
2023-01-20 20:27:38 +0000

    fix alignment bug on agents comp

--- components/Agents.vue
+++ components/Agents.vue
@@ -17,8 +17,8 @@
 
           <div class="hidden sm:block">
             <div class="border-b border-gray-200">
-              <nav class="flex space-x-3" aria-label="Tabs">
-                <a v-for="tab in tabs" :key="tab.name" :href="tab.href" :class="[tab.current ? 'border-cyan-500 text-cyan-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200', 'pl-5 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm']">
+              <nav class="flex flex justify-between" aria-label="Tabs">
+                <a v-for="tab in tabs" :key="tab.name" :href="tab.href" :class="[tab.current ? 'border-cyan-500 text-cyan-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200', 'pr-5 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm']">
                   {{ tab.name }}
                 </a>
               </nav>
@@ -36,7 +36,7 @@
             <a href="#" class="group block">
               <div class="flex items-center py-5 px-4 sm:py-6 sm:px-0">
                 <div class="flex min-w-0 flex-1 items-center">
-                  <div class="flex-shrink-0">
+                  <div class="flex-shrink-0 md:place-self-start">
                     <img class="h-12 w-12 rounded-full group-hover:opacity-75" :src="agents.imageUrl" alt="" />
                   </div>
                   <div class="min-w-0 flex-1 px-4">
