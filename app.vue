312f569382cdc435eaeb50df628d7c1da93c44b3
Daniel <52611355+kingdanie@users.noreply.github.com>
2023-01-20 00:39:31 +0000

    add agents and task components to app

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
+  <div class="min-h-full">
     <TopBar />
-    <NuxtWelcome />
+    <main class="mx-auto lg:grid lg:grid-cols-12">
+      <NavBar />
+      <div class="lg:col-span-10 w-full h-full bg-red-200 mt-7">
+        <div class="grid md:grid-cols-2 lg:grid-cols-3">
+          <Tasks />
+          <div>Map</div>
+         <Agents />
+        </div>
+      </div>
+    </main>
   </div>
 </template>
